5b6f54a891d6de00c6bfc0c7019bfc26564fbffb
Oblivioner339 <simone.piero32@gmail.com>
2025-05-07 16:46:39 +0200

    v2 github

--- src/components/allenamento/allenamento.component.ts
+++ src/components/allenamento/allenamento.component.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-allenamento',
   imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './allenamento.component.html',
-  styleUrls: []
+  styleUrls: ["allenamento.component.css"]
 })
 export class AllenamentoComponent {
   nickname: string = ''; // nuovo campo per il nickname
@@ -18,20 +18,25 @@ export class AllenamentoComponent {
       alert('Inserisci un nickname prima di continuare!');
       return;
     }
+
     const statPokemon: Record<string, any> = {
-      Flamix: {hp: 100, attacco: 10, difesa: 3},
-      Grooslime: {hp: 80, attacco: 15, difesa: 5},
-      Aquarock: {hp: 90, attacco: 13, difesa: 4}
+      Flamix: { hp: 100, attacco: 10, difesa: 3 },
+      Grooslime: { hp: 80, attacco: 15, difesa: 5 },
+      Aquarock: { hp: 90, attacco: 13, difesa: 4 }
     };
 
     const stat = statPokemon[nome];
-    localStorage.setItem('nickname', this.nickname); // SALVIAMO il nickname
+    localStorage.setItem('nickname', this.nickname);
     localStorage.setItem('nome', nome);
     localStorage.setItem('hp', stat.hp.toString());
     localStorage.setItem('attacco', stat.attacco.toString());
     localStorage.setItem('difesa', stat.difesa.toString());
 
+    // 👉 Aggiungi questa riga per salvare il nome dell'immagine
+    localStorage.setItem('img', nome.toLowerCase());
+
     location.href = '/battaglia';
   }
+
 }
 
--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {BackgroundService} from '../../services/background.service';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrls: []
+  styleUrls: ["home.component.css"]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private background:BackgroundService)
+  {
+    background.cambiaSfondo('sfondoAnimato');
+  }
+}
